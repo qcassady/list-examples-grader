ed8990ade84485bdcc6cec17885d105652f8c1af
qcassady <130010365+qcassady@users.noreply.github.com>
2023-05-24 17:47:00 -0700

    update

--- grade.sh
+++ grade.sh
@@ -9,18 +9,17 @@ git clone $1 student-submission
 echo 'Finished cloning'
 
 if [[ -f student-submission/ListExamples.java ]] then
-    cp -r student-submission grading-area
+    cp -r student-submission/ListExamples.java grading-area
     cp -r lib grading-area
-    cp -r lib grading-area/student-submission
+    cp -r TestListExamples.java grading-area
 else 
     echo "File not found!"
     exit 1
 fi
 
 cd grading-area
-javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" ./student-submission/ListExamples.java
-cd student-submission
-java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore ListExamples
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples
 
 # Draw a picture/take notes on the directory structure that's set up after
 # getting to this point
